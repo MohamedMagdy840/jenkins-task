50cbf8d17521ff40a7186aea2e26b05f778751eb
Mohamed Magdy AbuDeif <162097523+MohamedMagdy840@users.noreply.github.com>
2024-04-19 15:00:06 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Get the current date and time
+echo "Task Done by Mohamed MaGdy"
+echo "Jenkins Course"
 current_date_time=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Print the current date and time
 echo "Current date and time: $current_date_time"
+
